c575fdb058a9d3abc2fa280734a8212b8320d51a
Justin <justincyork@gmail.com>
2016-10-05 15:36:38 -0500

    Add create related and unrelated actions

--- popup/popup.js
+++ popup/popup.js
@@ -1,6 +1,7 @@
 var tabData, fsID,
     dev = chrome.app.getDetails().update_url ? false : true,
-    postUrl = dev ? 'http://dev4.wikitree.com/wiki/Special:MergeEdit' : 'https://httpbin.org/post';
+    mergeEditUrl = dev ? 'http://dev4.wikitree.com/wiki/Special:MergeEdit' : 'https://httpbin.org/post',
+    editFamilyUrl = dev ? 'http://dev4.wikitree.com/index.php?action=editfamily' : 'https://httpbin.org/post';
 
 // Initiate the scraping
 chrome.runtime.getBackgroundPage(function(background){
@@ -28,39 +29,52 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 // Setup button listeners
 //
 
-document.getElementById('fs-connections-btn').addEventListener('click', fsConnect);
-
 document.getElementById('update-existing-btn').addEventListener('click', updateExisting);
+document.getElementById('create-related-btn').addEventListener('click', createRelated);
+document.getElementById('create-unrelated-btn').addEventListener('click', createUnrelated);
+document.getElementById('fs-connections-btn').addEventListener('click', fsConnect);
 
 //
 // Input listeners so that forms are submitted on enter
 //
-document.getElementById('update-existing-wt-id').addEventListener('keypress', function(event){
-  if(enterPressed(event)){
-    updateExisting();
-  }
-});
-
-document.getElementById('fs-connections-wt-id').addEventListener('keypress', function(event){
-  if(enterPressed(event)){
-    fsConnect();
-  }
-});
-
 //
-// Helper methods
-//
+document.getElementById('update-existing').addEventListener('keypress', enterListener(updateExisting));
+document.getElementById('create-related').addEventListener('keypress', enterListner(createRelated));
+document.getElementById('create-unrelated').addEventListener('keypress', enterListner(createUnrelated));
+document.getElementById('fs-connections').addEventListener('keypress', enterListner(fsConnect));
 
+/**
+ * Update an existing person by POSTing gedcomx data to the MergeEdit page
+ */
 function updateExisting(){
   var wtID = document.getElementById('update-existing-wt-id').value;
-  postData(postUrl, wtID, tabData.genscrape);
+  postData(mergeEditUrl, wtID, tabData.genscrape);
 }
 
+/**
+ * Open the EditFamilySearch page for the given WikiTree profile
+ */
 function fsConnect(){
   var wtID = document.getElementById('fs-connections-wt-id').value;
   chrome.tabs.create({
     url: 'http://www.wikitree.com/index.php?title=Special:EditFamilySearch&action=viewUser&user_name=' + wtID + '&fs_id=' + fsID
   });
+}
+
+/**
+ * Create a new profile related to the given WikiTree profile
+ */
+function createRelated(){
+  var wtID = document.getElementById('create-related-wt-id').value,
+      relation = document.getElementById('create-related-relation').value;
+  postData(editFamilyUrl + '&title=N&w=' + relation + '&title=' + wtID, '', tabData.genscrape);
+}
+
+/**
+ * Create a new profile related to the given WikiTree profile
+ */
+function createUnrelated(){
+  postData(editFamilyUrl + '&w=new', '', tabData.genscrape);
 }
 
 /**
@@ -88,14 +102,31 @@ function postData(url, profileId, data){
   $form.action = url;
 
   // Decide whether we have a user ID or a profile name
-  if(parseInt(profileId, 10)){
-    $wtId.value = profileId;
-  } else {
-    $wtUsername.value = profileId;
+  if(profileId){
+    if(parseInt(profileId, 10)){
+      $wtId.value = profileId;
+    } else {
+      $wtUsername.value = profileId;
+    }
   }
 
   document.getElementById('postData').value = JSON.stringify(data);
   $form.submit();
+}
+
+/**
+ * Setup an event listener that calls the given function when
+ * an enter keypress event is detected
+ *
+ * @param {Function} func
+ * @return {Function} listener
+ */
+function enterListener(func){
+  return function(event){
+    if(enterPressed(event)){
+      func();
+    }
+  };
 }
 
 /**
